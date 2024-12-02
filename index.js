7ab7d4a95520794686b2e9d378d1be09f56546f7
AlexHacker555 <dogalexandre123@gmail.com>
2024-12-02 18:11:19 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
-let count = 1;
-let v = document.getElementById("adder");
-let ch1 = document.getElementById("Counter");
-ch1.textContent = count;
-v.value = 1;
-document.getElementById("plus").addEventListener('click', () => {
-    count += +v.value;
-    ch1.textContent = count;
+const display = document.getElementById("display");
+document.getElementById("1").addEventListener("click", () => {
+display.textContent += "1"
+document.getElementById('invis').textContent = "";
 })
-document.getElementById("minus").addEventListener('click', () => {
-    count -= +v.value;
-    ch1.textContent = count;
+document.getElementById("2").addEventListener("click", () => {
+display.textContent += "2"
+document.getElementById('invis').textContent = "";
 })
-document.getElementById("mult").addEventListener('click', () => {
-    count *= +v.value;
-    ch1.textContent = count;
+document.getElementById("3").addEventListener("click", () => {
+display.textContent += "3"
+
 })
-document.getElementById("devide").addEventListener('click', () => {
-    if (!+v.value == 0){
-        count /= +v.value;
-        ch1.textContent = count;
-    }
+document.getElementById("4").addEventListener("click", () => {
+display.textContent += "4"
+document.getElementById('invis').textContent = "";
 })
-document.getElementById("reset").addEventListener("click", () => {
-    v.value = 1;
-    count = 1;
-    ch1.textContent = count;
+document.getElementById("5").addEventListener("click", () => {
+display.textContent += "5"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("6").addEventListener("click", () => {
+document.getElementById('invis').textContent = "";
+display.textContent += "6"
+})
+document.getElementById("7").addEventListener("click", () => {
+display.textContent += "7"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("8").addEventListener("click", () => {
+display.textContent += "8"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("9").addEventListener("click", () => {
+display.textContent += "9"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("0").addEventListener("click", () => {
+display.textContent += "0"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("add").addEventListener("click", () => {
+display.textContent += "+"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("mul").addEventListener("click", () => {
+display.textContent += "*"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("dev").addEventListener("click", () => {
+display.textContent += "/"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("sub").addEventListener("click", () => {
+display.textContent += "-"
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("dot").addEventListener("click", () => {
+display.textContent += "."
+document.getElementById('invis').textContent = "";
+})
+document.getElementById("EQU").addEventListener("click", () => {
+try {
+display.textContent = eval(display.textContent)
+document.getElementById('invis').textContent = ""
+}
+catch (error) {
+display.textContent = ""
+document.getElementById('invis').textContent = "error";
+}
+
+})
+document.getElementById("RESET").addEventListener("click", () => {
+display.textContent = ""
+document.getElementById('invis').textContent = "?";
 })
